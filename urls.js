a96fef1cf52ddda92b5879626c029f5f4b0e33dc
Antoine <antoine.devillers01@gmail.com>
2024-10-02 15:24:37 +0200

    modif urls

--- urls.js
+++ urls.js
@@ -1,7 +1,7 @@
 // Insert the backend URL given by Vercel in this variable
-const BACKEND_URL = '';
+const BACKEND_URL = 'https://vercel-mymoviz-backend.vercel.app/';
 
 // Insert the frontend URL given by Vercel in this variable
-const FRONTEND_URL = '';
+const FRONTEND_URL = 'https://vercel-mymoviz-frontend.vercel.app/';
 
 module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
